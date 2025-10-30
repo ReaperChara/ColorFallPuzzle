aa51cd9dc389ee97d8094140afbd74b92e89e245
ReaperChara <Corefrisk119@gmail.com>
2025-10-30 16:45:31 +0300

    Add Block class with properties and clone method

--- Models/Block.cs
+++ Models/Block.cs
@@ -1,1 +1,18 @@
+namespace ColorFallPuzzle.Models;
 
+public class Block
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public SKColor Color { get; set; }
+
+    public Block(int x, int y, SKColor color)
+    {
+        X = x;
+        Y = y;
+        Color = color;
+    }
+
+    // Kopya oluştur (hareket için)
+    public Block Clone() => new Block(X, Y, Color);
+}
