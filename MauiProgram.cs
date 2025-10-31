77bf0b5f11202cd017a045bb62514e6f388634df
ReaperChara <Corefrisk119@gmail.com>
2025-10-31 16:40:49 +0300

    Add comment for CrossMTAdmob usage in MauiProgram
    Added a comment for CrossMTAdmob initialization.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,6 +1,6 @@
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
-using Plugin.MauiMTAdmob;
+using Plugin.MauiMTAdmob;  // EKLE: CrossMTAdmob için
 
 namespace ColorFallPuzzle;
 
