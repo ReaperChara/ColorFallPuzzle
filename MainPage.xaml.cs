e8de682bb91ec85c46e43dcd6881ca133fd9dfc8
ReaperChara <Corefrisk119@gmail.com>
2025-10-31 16:46:22 +0300

    Refactor MainPage to use MauiApp and remove obsolete code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,67 +1,24 @@
-using Microsoft.Maui.Dispatching;  // EKLE: Dispatcher için
-using SkiaSharp;  // EKLE: SKColors için
-using SkiaSharp.Views.Maui;
-using SkiaSharp.Views.Maui.Controls;
-using ColorFallPuzzle.Services;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Plugin.MauiMTAdmob;  // EKLE
 
 namespace ColorFallPuzzle;
 
-public partial class MainPage : ContentPage
+public static class MauiProgram
 {
-    private GameManager _gameManager;
-    private double _canvasWidth, _canvasHeight;
-
-    public MainPage()
+    public static MauiApp CreateMauiApp()
     {
-        InitializeComponent();
-        _gameManager = new GameManager();
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-        // Dispatcher ile timer (obsolete fix)
-        Dispatcher.StartTimer(TimeSpan.FromMilliseconds(16), () =>
-        {
-            _gameManager.Update();
-            GameCanvas.InvalidateSurface();
-            return true;
-        });
+        CrossMTAdmob.Current.Init();  // Şimdi tanınıyor
 
-        // Dokunma
-        var tap = new TapGestureRecognizer();
-        tap.Tapped += OnTapped;
-        GameCanvas.GestureRecognizers.Add(tap);
-    }
-
-    private void OnCanvasPaint(object sender, SKPaintSurfaceEventArgs e)  // XAML ile eşleşsin
-    {
-        _canvasWidth = e.Info.Width;
-        _canvasHeight = e.Info.Height;
-
-        var canvas = e.Surface.Canvas;
-        canvas.Clear(SKColors.Black);
-        _gameManager.Draw(canvas, (int)_canvasWidth, (int)_canvasHeight);
-
-        ScoreLabel.Text = $"Score: {_gameManager.Score}";
-    }
-
-    private void OnTapped(object? sender, TappedEventArgs e)  // Nullable sender (warning fix)
-    {
-        var pos = e.GetPosition(GameCanvas);
-        if (!pos.HasValue) return;
-
-        double x = pos.Value.X;
-        double y = pos.Value.Y;
-        double thirdX = _canvasWidth / 3;
-        double topY = _canvasHeight * 0.3;
-
-        if (x < thirdX)
-            _gameManager.MoveLeft();
-        else if (x < 2 * thirdX)
-            _gameManager.DropFast();
-        else
-            _gameManager.MoveRight();
-
-        if (y < topY)
-            _gameManager.Rotate();
-
-        GameCanvas.InvalidateSurface();
+        return builder.Build();
     }
 }
