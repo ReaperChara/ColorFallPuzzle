721f49a229660c3a93d883b8efc7fa50414b2340
ReaperChara <Corefrisk119@gmail.com>
2025-11-01 16:29:11 +0300

    Update MainActivity.cs

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,16 +1,15 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Plugin.MauiMTAdmob;  // DOĞRU using
 
 namespace ColorFallPuzzle;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Bundle? savedInstanceState)  // Nullable Bundle? (warning fix)
+    protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
-        CrossMauiMTAdmob.Current.Init(this);  // DOĞRU init
+        // Plugin otomatik init olur, ekstra kod gerekmez
     }
 }
