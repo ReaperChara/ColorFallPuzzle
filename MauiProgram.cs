83d43801009e8012cafb11d722ad37b9034d922e
ReaperChara <Corefrisk119@gmail.com>
2025-11-01 16:08:57 +0300

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,13 +11,12 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiMTAdmob()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
-        Admob.Initialize();  // Plugin init
 
         return builder.Build();
     }
